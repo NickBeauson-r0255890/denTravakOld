33ccb08e7428be63ba614688236e847acdef59b2
NickBeauson <NickBeauson-r0255890@github.com>
2018-11-06 18:50:37 +0100

    Created SandwichBuilder

--- src/main/java/model/Sandwich.java
+++ src/main/java/model/Sandwich.java
@@ -3,27 +3,24 @@ package model;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class Sandwich {
 
-    private String Name;
+    private UUID id;
+    private String name;
     private BigDecimal price;
-    private String Ingredients;
+    private String ingredients;
 
-    public Sandwich(){
-    }
-
-    public Sandwich(String name, BigDecimal price ){
-        this.setName(name);
-        this.setPrice(price);
+    private Sandwich(){
     }
 
     public String getName() {
-        return Name;
+        return name;
     }
 
     public void setName(String name) {
-        Name = name;
+        this.name = name;
     }
 
     public BigDecimal getPrice() {
@@ -34,7 +31,52 @@ public class Sandwich {
         this.price = price;
     }
 
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public static class SandwichBuilder{
+
+        private UUID id;
+        private String name;
+        private BigDecimal price;
+        private String ingredients;
+
+        public SandwichBuilder(UUID id){
+            this.id = id;
+        }
+
+        public SandwichBuilder withName(String name) {
+            this.name = name;
+           return this;
+        }
+
+        public SandwichBuilder withPrice(BigDecimal price) {
+            this.price = price;
+           return this;
+        }
+
+        public SandwichBuilder withIngredients(String ingredients) {
+            this.ingredients = ingredients;
+           return this;
+        }
+
+        public Sandwich build(){
+            Sandwich sandwich = new Sandwich();
+            sandwich.name = this.name;
+            sandwich.price = this.price;
+            sandwich.ingredients = this.ingredients;
+            return sandwich;
+        }
+        
+    }
 
 
 
 }
+
+
--- src/main/java/ui/TestUI.java
+++ src/main/java/ui/TestUI.java
@@ -5,12 +5,17 @@ import model.Order;
 import model.Sandwich;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 public class TestUI {
     public static void main(String[]args){
-        Sandwich testSandwich = new Sandwich("cheese",new BigDecimal(3.2).setScale(2,BigDecimal.ROUND_HALF_UP));
-        Order testOrder = new Order("Wrap",testSandwich.getName(), testSandwich.getPrice(),"0475135268");
 
-        System.out.println("You ordered a " + testOrder.getSandwichName() + " " + testOrder.getBreadtype() + " for " + testSandwich.getPrice() + " euro.");
+        Sandwich buildedSandwich = new Sandwich.SandwichBuilder(UUID.randomUUID())
+                .withName("cheese")
+                .withPrice(new BigDecimal(3.2).setScale(1,BigDecimal.ROUND_HALF_UP))
+                .withIngredients("Cheese, eggs, letuce, tomato, mayo")
+                .build();
+
+        System.out.println("You ordered a " + buildedSandwich.getName() + " sandwich for " + buildedSandwich.getPrice() + " euro.");
     }
 }
